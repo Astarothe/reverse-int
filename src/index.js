4d7a31600381620b214dc02561f78ac774cdb87e
Mishel Savich <minatosavich@gmail.com>
2020-10-21 19:05:17 +0300

    add task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0){
+        let reverse = (String(n).split("").reverse().join(""));
+        console.log(Number(reverse));
+        return Number(reverse);
+    } else if (n < 0){
+        n = n * -1;
+        let reverse = (String(n).split("").reverse().join(""));
+        console.log(Number(reverse));
+        return Number(reverse);
+    }
+
+
 }
